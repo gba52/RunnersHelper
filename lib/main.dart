2395b40e226dd142e882a52ab757a5cc0454417f
AP\GBates <greg_bates@trimble.com>
2022-10-07 15:51:42 +1300

    Background colors

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,22 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Runners Helper Application'),
         ),
-        body: const Center(
-          child: Text('This will help you with all your running needs',
-            style: TextStyle(fontSize: 24),) ,
-        ),
+        body: Container(
+          decoration: BoxDecoration(
+            color:Colors.orange,
+            gradient: LinearGradient(
+              begin: Alignment(0.0, 0.5),
+              end: Alignment(0.0, 1.0),
+              colors: [Colors.purple.shade50, Colors.purple.shade500] ),
+            shape: BoxShape.rectangle,
+            borderRadius: BorderRadius.zero
+          )
+        )
       ),
     );
   }
 }
+
+
+//const Color.fromARGB(1, 44, 56, 74)
+//const Color.fromARGB(1, 245, 124, 1)
