36bfa780fb1a0b7e292f9e1d3e3d546c556a1f56
AP\GBates <greg_bates@trimble.com>
2022-10-28 16:20:31 +1300

    Add tabs into the application.

--- lib/main.dart
+++ lib/main.dart
@@ -6,32 +6,44 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Runners Helper Application'),
-        ),
-        body: Container(
-          margin:EdgeInsets.all(50.0),
-          width: 300.0,
-          height: 300.0,
-          decoration: BoxDecoration(
-            color:Colors.orange,
-            gradient: LinearGradient(
-              begin: Alignment(0.0, 0.5),
-              end: Alignment(0.0, 1.0),
-              colors: [Colors.purple.shade50, Colors.purple.shade500] ),
-            image:DecorationImage(
-              image:NetworkImage('https://openclipart.org/image/400px/270138'),
-              fit:BoxFit.scaleDown,
+      title: 'App Runners Helper',
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+      ),
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("App bar runners helper"),
+            bottom: TabBar(tabs: <Widget>[
+              Tab(icon: Icon(Icons.account_balance), text: "Tab 1",),
+              Tab(icon: Icon(Icons.add_alarm_outlined), text: "Tab 2",),
+              Tab(icon: Icon(Icons.accessibility_outlined), text: "Tab 3",)
+            ],),
+          ),
+          body: TabBarView(children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage('https://openclipart.org/image/400px/270138'),
+                  fit: BoxFit.cover),
+              ),
+            ),Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage('https://openclipart.org/image/400px/323666'),
+                  fit: BoxFit.cover),
+              ),
+            ),Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage('https://openclipart.org/image/400px/183633'),
+                  fit: BoxFit.cover),
+              ),
             ),
-            shape: BoxShape.circle
-          )
-        )
+          ],),
+        ),
       ),
     );
   }
 }
-
-
-//const Color.fromARGB(1, 44, 56, 74)
-//const Color.fromARGB(1, 245, 124, 1)
