20f0b2523bb8868f29152acc0b700f6c994a7192
AP\GBates <greg_bates@trimble.com>
2022-11-26 16:15:37 +1300

    Update colors and page names.

--- lib/main.dart
+++ lib/main.dart
@@ -8,17 +8,21 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'App Runners Helper',
       theme: ThemeData(
-        primarySwatch: Colors.yellow,
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.red,
+            brightness: Brightness.light,
+
+            primary: Colors.blue)
       ),
       home: DefaultTabController(
         length: 3,
         child: Scaffold(
           appBar: AppBar(
-            title: Text("App bar runners helper"),
+            title: Text("Runners helper"),
             bottom: TabBar(tabs: <Widget>[
-              Tab(icon: Icon(Icons.account_balance), text: "Tab 1",),
-              Tab(icon: Icon(Icons.add_alarm_outlined), text: "Tab 2",),
-              Tab(icon: Icon(Icons.accessibility_outlined), text: "Tab 3",)
+              Tab(icon: Icon(Icons.calculate_outlined), text: "Split Calc",),
+              Tab(icon: Icon(Icons.question_mark), text: "Unused",),
+              Tab(icon: Icon(Icons.question_mark), text: "Unused",)
             ],),
           ),
           body: TabBarView(children: <Widget>[
